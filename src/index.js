197b2fa76c801c8373dad5ce1567a528f4946a9e
Mahred1 <mredwan.kemal@gmail.com>
2025-02-05 17:06:18 +0300

    fnished creating Pizza.co

--- src/index.js
+++ src/index.js
@@ -1,17 +1,133 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizzan Salamio",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Welcome to our React pizza</h1>
+    </header>
+  );
+}
+
+function Menu() {
+  let pizzas = pizzaData.length;
+
+  
+
+  return (
+    <main className="menu">
+      <h2>our menu</h2>
+      {pizzas > 0 ? (
+       <>
+       <p>Check out our creatuve dishes.</p>
+        <ul className="pizzas">
+          {pizzaData.map((pizza) => (
+            <Pizza pizzaObj={pizza} />
+          ))}
+        </ul>
+        </>
+      ) : (
+        <p>WE currenty dont have anything on our menu!</p>
+      )}
+    </main>
+  );
+}
+
+function Pizza({pizzaObj}) {
+  return (
+    <li className={`pizza ${pizzaObj.soldOut?"sold-out":""}`}>
+      <img src={pizzaObj.photoName} alt={pizzaObj.name}></img>
+      <div>
+        <h2>{pizzaObj.name}</h2>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.soldOut?"SOLD OUT":pizzaObj.price + "$"}</span>
+      </div>
+    </li>
+  );
+}
+function Footer() {
+  const hour = new Date().getHours();
+  let openHour = 12;
+  let closeHour = 22;
+  let isOpen = hour >= openHour && hour <= closeHour;
+  return (
+    <footer className="footer">
+      {isOpen ? (
+       <Order closeHour={closeHour}/>
+      ) : (
+        <p>
+          We'll be open between {openHour}:00 and {closeHour}:00 
+        </p>
+      )}
+    </footer>
+  );
+}
+
+function Order({closeHour}){
+  return  <div className="order">
+  <p>We are curruntly open until {closeHour}:00, Order now!</p>
+<button className="btn">Order Now</button>
+</div>
+}
+const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
